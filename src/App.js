939330700fd395edb9eca094b62ae566f8ded16e
Joel <sippun@users.noreply.github.com>
2022-12-13 17:55:42 -0800

    Create hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import useApi from './useApi';
+
 function App() {
+  const { loading, data } = useApi('https://jsonplaceholder.typicode.com/users');
+
+  if (loading) return <h1>Loading</h1>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data fetched successfully</h1>
+      {JSON.stringify(data)}
     </div>
   );
 }
